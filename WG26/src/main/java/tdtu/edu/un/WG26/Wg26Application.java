4d431b4edb347e0688c6d75abc870d328c13f870
Khoa Tran <tran.dangkhoa047@gmail.com>
2022-11-10 16:39:49 +0700

    first commit

---
+++ WG26/src/main/java/tdtu/edu/un/WG26/Wg26Application.java
@@ -0,0 +1,13 @@
+package tdtu.edu.un.WG26;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wg26Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Wg26Application.class, args);
+	}
+
+}
---
+++ WG26/src/test/java/tdtu/edu/un/WG26/Wg26ApplicationTests.java
@@ -0,0 +1,13 @@
+package tdtu.edu.un.WG26;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wg26ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
