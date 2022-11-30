5ced5a34c84465539e9270b38592de4c6fef35cb
KingSlyt-import <kingslyt36@gmail.com>
2022-11-30 18:05:59 +0700

    add homepage mapping on IndexController.java

--- WG26/src/main/java/tdtu/edu/un/WG26/web/IndexController.java
+++ WG26/src/main/java/tdtu/edu/un/WG26/web/IndexController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
-	@GetMapping("/")
+	@GetMapping("/home")
 	public String getHomePage() {
-		return "homePage";
+		return "home";
 	}
 	
 }
